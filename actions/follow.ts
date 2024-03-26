6f966e5f901df0651a617f843c8be4275fc6fda6
Rakshath U Shetty <eng20cs0278.rakshath.u.shetty@gmail.com>
2024-03-27 01:42:04 +0530

    following concepts

---
+++ actions/follow.ts
@@ -0,0 +1,41 @@
+"use server";
+
+import { followUser, unfollowUser } from "@/lib/follow-service";
+import { revalidatePath } from "next/cache";
+
+export const onFollow = async (id: string) => {
+    try {
+        // console.log("I am the same as an API Call");
+        const followedUser = await followUser(id);
+        
+        //refresh recommended users
+        revalidatePath("/");
+
+        //refresh the username of user i followed
+        if (followedUser) {
+            revalidatePath(`/${followedUser.following.username}`);
+        }
+
+        return followedUser;
+        
+    } catch (error) {
+        throw new Error("Internal Error");
+    }
+}
+
+export const onUnfollow = async (id: string) => {
+    try {
+        const unfollowedUser = await unfollowUser(id);
+        console.log(unfollowedUser);
+
+        revalidatePath("/");
+
+        if (unfollowedUser) {
+            revalidatePath(`/${unfollowedUser.following.username}`);
+        }
+
+        return unfollowedUser;
+    } catch (error) {
+        throw new Error("Internal Error");
+    }
+}
--- lib/recommended-service.ts
+++ lib/recommended-service.ts
@@ -4,12 +4,37 @@ import { getSelf } from "./auth-service";
 // recent created users first
 export const getRecommended = async () => {
     // await new Promise(resolve => setTimeout(resolve, 5000));
+    let userId;
 
-    const users = await db.user.findMany({
-        orderBy: {
-            createdAt: "desc"
-        }
-    })
+    try {
+        const self = await getSelf();
+        userId = self.id;
+    } catch {
+        userId = null;
+    }
+
+    let users = [];
+
+    // if user is me, dont show me in recommended list
+    if(userId) {
+        users = await db.user.findMany({
+            where: {
+                NOT: {
+                    id: userId,
+                }
+            },
+            orderBy: {
+                createdAt: "desc"
+            }
+        })
+    }
+    else {
+        users = await db.user.findMany({
+            orderBy: {
+                createdAt: "desc"
+            }
+        })
+    }
 
     return users;
 }
