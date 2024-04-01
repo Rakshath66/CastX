9e0d09bc7d30df33422a6e20787428df951ad511
Rakshath U Shetty <eng20cs0278.rakshath.u.shetty@gmail.com>
2024-04-02 01:54:47 +0530

    updating the features of navbar - search component

--- app/(browse)/_components/navbar/search.tsx
+++ app/(browse)/_components/navbar/search.tsx
@@ -18,7 +18,7 @@ export const Search = () => {
     if(!value) return;
 
     const url = qs.stringifyUrl({
-      url: "/",
+      url: "/search",
       query: { term: value },
     }, { skipEmptyString: true });
 
@@ -34,20 +34,20 @@ export const Search = () => {
         onSubmit={onSubmit}
         className="relative w-full lg:w-[400px] flex items-center"
       >
-        <Input 
-           value={value}
-           onChange={(e) => setValue(e.target.value)}
-           placeholder="Search"
-           className="rounded-r-none focus-visible:ring-0 focus-visible:ring-transparent focus-within:ring-offset-0"
+        <Input
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          placeholder="Search"
+          className="rounded-r-none focus-visible:ring-0 focus-visible:ring-transparent focus-visible:ring-offset-0"
         />
         {value && (
-          <X 
-             className="absolute top-2.5 right-14 h-5 w-5 text-muted-foreground cursor-pointer hover:opacity-75 transition"
-             onClick={onClear}
+          <X
+            className="absolute top-2.5 right-14 h-5 w-5 text-muted-foreground cursor-pointer hover:opacity-75 transition"
+            onClick={onClear}
           />
         )}
-
-        <Button
+        
+        <Button 
           type="submit"
           size="sm"
           variant="secondary"
