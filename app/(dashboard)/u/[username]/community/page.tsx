b14513e07fbcaaf5ad7d1bdc6fc9dc594b6847b4
Rakshath U Shetty <eng20cs0278.rakshath.u.shetty@gmail.com>
2024-04-02 00:51:13 +0530

    updated-block service

--- app/(dashboard)/u/[username]/community/page.tsx
+++ app/(dashboard)/u/[username]/community/page.tsx
@@ -1,22 +1,33 @@
-import { Payment } from "./_components/columns";
+import {format } from "date-fns";
 
-async function getData(): Promise<Payment[]> {
-    // Fetch data from your API here.
-    return [
-      {
-        id: "728ed52f",
-        amount: 100,
-        status: "pending",
-        email: "m@example.com",
-      },
-      // ...
-    ]
+import { getBlockedUsers } from "@/lib/block-service";
+
+import { DataTable } from "./_components/data-table";
+import { columns } from "./_components/columns";
+
+// show blocked users in tables
+// get data and format it
+const CommunityPage = async () => {
+    const blockedUsers = await getBlockedUsers();
+  
+    const formattedData = blockedUsers.map((block) => ({
+      ...block,
+      userId: block.blocked.id,
+      imageUrl: block.blocked.imageUrl,
+      username: block.blocked.username,
+      createdAt: format(new Date(block.blocked.createdAt), "dd/MM/yyyy"),
+    }));
+  
+    return ( 
+      <div className="p-6">
+        <div className="mb-4">
+          <h1 className="text-2xl font-bold">
+            Community Settings
+          </h1>
+        </div>
+        <DataTable columns={columns} data={formattedData} />
+      </div>
+     );
 }
 
-export const CommunityPage = () => {
-  return (
-     <div>
-        page
-     </div>
-  );
-};
+export default CommunityPage;
