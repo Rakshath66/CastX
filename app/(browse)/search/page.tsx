d1d4c2979fc38ffe0831de311eab71a53bb2588d
Rakshath U Shetty <eng20cs0278.rakshath.u.shetty@gmail.com>
2024-04-01 23:57:46 +0530

    serach bar update

--- app/(browse)/search/page.tsx
+++ app/(browse)/search/page.tsx
@@ -1,7 +1,25 @@
-export const SearchPage = () => {
-  return (
-     <div>
-        page
-     </div>
-  );
+import { Suspense } from "react";
+import { redirect } from "next/navigation";
+
+
+interface SearchPageProps {
+  searchParams: { //params:{username} - access username from dynamic url - same way alternative to access searchParams
+    term?: string; //query in the url
+  };
 };
+
+const SearchPage = ({
+    searchParams,
+}: SearchPageProps) => {
+    if (!searchParams.term) {
+      redirect("/");
+    }
+
+    return ( 
+        <div className="h-full p-8 max-w-screen-2xl mx-auto">
+          
+        </div>
+    );
+};
+     
+export default SearchPage;
