23fa3945da6137e5c131539c0223f5fa029122ec
Rakshath U Shetty <eng20cs0278.rakshath.u.shetty@gmail.com>
2024-04-02 00:16:06 +0530

    result card skeleton

---
+++ app/(browse)/search/_components/result-card.tsx
@@ -0,0 +1,15 @@
+export const ResultCard = () => {
+  return (
+     <div>
+        result-card
+     </div>
+  );
+};
+
+export const ResultCardSkeleton = () => {
+  return (
+     <div>
+        result-card
+     </div>
+  );
+};
--- app/(browse)/search/_components/results.tsx
+++ app/(browse)/search/_components/results.tsx
@@ -1,6 +1,10 @@
+import { getSearch } from "@/lib/search-service";
 import { Skeleton } from "@/components/ui/skeleton";
 
-
+import { 
+  ResultCard, 
+  ResultCardSkeleton
+} from "./result-card";
 
 interface ResultsProps {
   term?: string;
@@ -9,12 +13,25 @@ interface ResultsProps {
 export const Results = async ({
     term,
 }: ResultsProps) => {
+    const data = await getSearch(term);
 
     return (
         <div>
           <h2 className="text-lg font-semibold mb-4">
             Results for term &quot;{term}&quot;
           </h2>
+
+          {data.length === 0 && (
+            <p className="text-muted-foreground text-sm">
+              No results found. Try searching for something else
+            </p>
+          )}
+          
+          <div className="flex flex-col gap-y-4">
+            {data.map((result) => (
+              <ResultCard data={result} key={result.id} />
+            ))}
+          </div>
         </div>
     );
 };
@@ -23,6 +40,11 @@ export const ResultsSkeleton = () => {
     return (
       <div>
         <Skeleton className="h-8 w-[290px] mb-4" />
+        <div className="flex flex-col gap-y-4">
+          {[...Array(4)].map((_, i) => (
+            <ResultCardSkeleton key={i} />
+          ))}
+        </div>
       </div>
     );
 };
