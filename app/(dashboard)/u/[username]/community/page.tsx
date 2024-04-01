9307dacfce7961e09e4a3a5de3bf4c9cb4ba64b1
Rakshath U Shetty <eng20cs0278.rakshath.u.shetty@gmail.com>
2024-04-02 00:42:08 +0530

    shadcn ui setup for tables

--- app/(dashboard)/u/[username]/community/page.tsx
+++ app/(dashboard)/u/[username]/community/page.tsx
@@ -1,3 +1,18 @@
+import { Payment } from "./_components/columns";
+
+async function getData(): Promise<Payment[]> {
+    // Fetch data from your API here.
+    return [
+      {
+        id: "728ed52f",
+        amount: 100,
+        status: "pending",
+        email: "m@example.com",
+      },
+      // ...
+    ]
+}
+
 export const CommunityPage = () => {
   return (
      <div>
