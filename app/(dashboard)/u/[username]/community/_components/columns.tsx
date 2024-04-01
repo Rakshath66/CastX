b9a6f4dedc94ac9111aedd620be5980e7da12093
Rakshath U Shetty <eng20cs0278.rakshath.u.shetty@gmail.com>
2024-04-02 01:08:55 +0530

    added feature of community setting

--- app/(dashboard)/u/[username]/community/_components/columns.tsx
+++ app/(dashboard)/u/[username]/community/_components/columns.tsx
@@ -1,27 +1,60 @@
 "use client"
 
-import { ColumnDef } from "@tanstack/react-table"
+import { ArrowUpDown } from "lucide-react";
+import { ColumnDef } from "@tanstack/react-table";
+
+import { Button } from "@/components/ui/button";
+import { UserAvatar } from "@/components/user-avatar";
+
+// import { UnblockButton } from "./unblock-button";
 
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
-export type Payment = {
-  id: string
-  amount: number
-  status: "pending" | "processing" | "success" | "failed"
-  email: string
+export type BlockedUser = {
+  id: string;
+  userId: string;
+  imageUrl: string;
+  username: string;
+  createdAt: string;
 }
 
-export const columns: ColumnDef<Payment>[] = [
+//cell formatting, pagination(previous-next page), sorting(sort username by ascending), filtering(by search)
+export const columns: ColumnDef<BlockedUser>[] = [
   {
-    accessorKey: "status",
-    header: "Status",
+    accessorKey: "username",
+    header: ({ column }) => (
+      <Button
+        variant="ghost"
+        onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+      >
+        Username
+        <ArrowUpDown className="ml-2 h-4 w-4" />
+      </Button>
+    ),
+    cell: ({ row }) => (
+      <div className="flex items-center gap-x-4">
+        <UserAvatar 
+          username={row.original.username} 
+          imageUrl={row.original.imageUrl}
+        />
+        <span>{row.original.username}</span>
+      </div>
+    )
   },
   {
-    accessorKey: "email",
-    header: "Email",
+    accessorKey: "createdAt",
+    header: ({ column }) => (
+      <Button
+        variant="ghost"
+        onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+      >
+        Date blocked
+        <ArrowUpDown className="ml-2 h-4 w-4" />
+      </Button>
+    ),
   },
   {
-    accessorKey: "amount",
-    header: "Amount",
+    id: "actions",
+    // cell: ({ row }) => <UnblockButton userId={row.original.userId} />
   },
 ]
