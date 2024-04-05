fcdaf21ac039c845e0b89576f58489248283720a
Rakshath U Shetty <eng20cs0278.rakshath.u.shetty@gmail.com>
2024-04-05 13:22:37 +0530

    updated the content

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
           <ThemeProvider
             attribute='class'
             forcedTheme='dark'
-            storageKey='gamehub-theme'
+            storageKey='castx-theme'
           >
             <Toaster theme="light" position="bottom-center" />
             {children}
--- components/stream-player/chat-header.tsx
+++ components/stream-player/chat-header.tsx
@@ -8,14 +8,14 @@ import { VariantToggle } from "./variant-toggle";
 export const ChatHeader = () => {
   return (
      <div className="relative p-3 border-b">
-        {/* TODO: Toggle Chat Sidebar */}
+        {/* todo: Toggle Chat Sidebar */}
         <div className="absolute left-2 top-2 hidden lg:block">
             <ChatToggle />
         </div>
         <p className="font-semibold text-primary text-center">
             Stream Chat
         </p>
-        {/* TODO: Toggle Chat Community */}
+        {/* todo next: Toggle Chat Community */}
         <div className="absolute right-2 top-2">
             <VariantToggle />
         </div>
